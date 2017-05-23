a746b4ad13144231310a50054d70e5230025fb53
Aaron Lee <beardshadow@gmail.com>
2017-05-23 16:42:15 -0700

    up to simon says part where first little word needs to be capitalized

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,12 @@
 #write your code here
+def add(num1, num2)
+	num1 + num2
+end
+
+def subtract(num1, num2)
+	num1 - num2
+end
+
+def sum(arr)
+	arr.empty? ? 0 : arr.inject(:+)
+end
--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,30 @@
 #write your code here
+def echo(string)
+	string
+end
+
+def shout(string)
+	string.upcase
+end
+
+def repeat(string, times=2)
+	((string + ' ' ) * times).strip
+end
+
+def start_of_word(string, num)
+	string[0...num]
+end
+
+def first_word(string)
+	string.split(' ').first 
+end
+
+#def titleize(string)
+#	little_words = ['a', 'an', 'in', 'on','of','to','the']
+#	string.split(' ').map{ |word| little_words.include?(word) && (string.split(' ')[0] != word) ? word : word.capitalize }.join(' ')
+#end
+
+def titleize(string)
+	little_words = ['a', 'an', 'and', 'over', 'in', 'on','of','to','the']
+	string.split(' ').map { |word| (word != string.split(' ')[0]) && little_words.include?(word) ? word : word.capitalize}.join(' ')
+end
